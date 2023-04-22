cd6fbdff685db0987e598efc9d25141dc31d28af
mohamedamara1 <hamma.amara@gmail.com>
2023-04-22 13:22:42 +0200

    added lesson and attendance handlers

---
+++ webserver_student_attendance/attendance_handlers.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <WebServer.h>
+#include "credits.h"
+#include "web_page.h"
+#include <ArduinoJson.h>
+
+extern WebServer server;
+extern sqlite3 *db;
+
+extern int db_exec(sqlite3 *, const char *);
+
+void handleGetAttendances();
+void handleCreateAttendance();
+void handleDeleteAttendance();
+void handleUpdateAttendance();
---
+++ webserver_student_attendance/lesson_handlers.cpp
@@ -0,0 +1,33 @@
+#include "lesson_handlers.h"
+
+
+// Get all lessons
+void handleGetlessons() {
+DynamicJsonDocument doc(1024);
+  JsonArray array = doc.to<JsonArray>();
+  String sql = "SELECT * FROM lesson";
+  sqlite3_stmt *stmt;
+  if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+  {
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        JsonObject obj = array.createNestedObject();
+        obj["id"] = sqlite3_column_int(stmt, 0);
+        obj["course_id"] = sqlite3_column_int(stmt, 1);
+        obj["class_id"] = sqlite3_column_int(stmt, 2);
+        obj["teacher_id"] = sqlite3_column_int(stmt, 3);
+        obj["classroom_id"] = sqlite3_column_int(stmt, 4);
+        obj["date"] = sqlite3_column_text(stmt, 5);
+        obj["start_time"] = sqlite3_column_text(stmt, 6);        obj["start_time"] = sqlite3_column_int(stmt, 3);
+        obj["end_time"] = sqlite3_column_text(stmt, 7);
+
+    }
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+    server.send(200, "application/json", jsonStr);
+  }
+  else
+  {
+    server.send(400, "text/html", "FAIL");
+  }
+}
