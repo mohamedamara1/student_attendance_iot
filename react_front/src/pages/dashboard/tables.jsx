f4d7e379bf6bc7fa341636ffd319e9c51c004d7b
mohamedamara1 <hamma.amara@gmail.com>
2023-05-05 11:00:01 +0200

    Working on mqtt on teacher side

--- react_front/src/pages/dashboard/tables.jsx
+++ react_front/src/pages/dashboard/tables.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import mqtt from "precompiled-mqtt";
 
 import {
   Card,
@@ -18,6 +19,8 @@ export function Tables() {
   const [attendances, setAttendances] = useState([]);
   const [lessons, setLessons] = useState([]);
   const teacherId= localStorage.getItem("teacherId");
+  const [selectedLessonId, setSelectedLessonId] = useState();
+  const [mqttStatus, setMqttStatus] = useState("Connecting...");
 
   async function fetchLessonsTableData() {
     try {
@@ -35,6 +38,7 @@ export function Tables() {
 
   async function handleLessonClick(lessonId) {
     console.log(lessonId);
+    setSelectedLessonId(lessonId);
     try {
       const response = await fetch(`http://192.168.1.8/attendances/findByLessonId?lessonId=${lessonId}`);
       const filteredData = await response.json();
@@ -44,12 +48,64 @@ export function Tables() {
     }
   }
 
+  useEffect(() => {
+    // Connect to MQTT broker
+    const esp32Ip ="192.168.1.8"
+    const mqttBrokerPort = "1883"
+    const mosqittoURL = "mqtt://test.mosquitto.org:8080";
+    const espBrokerUrl = "mqtt://" + esp32Ip + ":" +mqttBrokerPort
+
+
+   // const client = mqtt.connect(URL);
+    
+    // Subscribe to presence/lessonId topic
+   // const topic = `presence/${lessonId}`;
+   const options = {
+    port: 1883,
+    protocol: 'ws'
+  };
+    const client = mqtt.connect(espBrokerUrl);
+    const topic = "presence";
+    client.on('connect', function () {
+      console.log("connected")
+      client.subscribe(topic, function (err) {
+        if (!err) {
+          client.publish(topic, 'Hello mqtt')
+          console.log("subscribed");
+        }
+      })
+    })
+
+  }, []);
+
+  const handleConnect = () => {
+    // Connect to MQTT broker
+    // ...
+    setMqttStatus("Connected");
+  };
+
+  const handleDisconnect = () => {
+    // Disconnect from MQTT broker
+    // ...
+    setMqttStatus("Disconnected");
+  };
+
+ 
   return (
     <div className="mt-12 mb-8 flex flex-col gap-12">
-     
-      <Table data={attendances} setAttendances={setAttendances} columns={attendancesTableColumns} />
-      <Table data={lessons} columns={lessonsTableColumns} handleRowClick={handleLessonClick} />          
-      
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-bold">MQTT Status: {mqttStatus}</h2>
+        <div className="flex gap-4">
+          <button>Connect</button>
+          <button >Disconnect</button>
+        </div>
+      </div>
+      <Table
+        data={attendances}
+        setAttendances={setAttendances}
+        columns={attendancesTableColumns}
+      />
+      <Table data={lessons} columns={lessonsTableColumns} handleRowClick={handleLessonClick} />
     </div>
   );
 }
